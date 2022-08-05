67eaa391a34e4e06a9b13c3d3fefff285a1fadf5
Bejaro <benjrose89@gmail.com>
2022-08-05 17:20:37 +0100

    Input working, no operators

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let displayContent = document.getElementById('screenContent').innerHTML;
+let displayContent = document.getElementById('screenContent');
+let currentValue = ["Calculator by Ben Rose"];
+if (currentValue.length > 15) currentValue.length = 15;
+
 const buttons = document.querySelectorAll('.calcButton');
 buttons.forEach (function (button){
     button.addEventListener('click', buttonClicked);
 });
 
+const numberButtons = document.querySelectorAll('.digit');
+
+for (var i = 0; i < numberButtons.length; i++) {     
+};
+
 function buttonClicked(clicked){
-    console.log(this.id);
+    console.log("Click: " + this.id);
     if (this.id == 'allclear'){
-        displayContent = "";
         clearScreen();
+        return;
+    }
+    if (this.classList.contains('digit')){
+        currentValue.push(this.innerHTML);
+        displayContent.innerHTML = currentValue.join("");
+        return;
     }
 }
 
 function clearScreen(){
-    displayContent = "";
-    console.log(displayContent);
+    displayContent.innerHTML = "";
+    currentValue.length = 0;
 }
